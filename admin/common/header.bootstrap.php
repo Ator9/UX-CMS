916a19f175109bd5938f3b09dc3071d231849f34
Ator9 <sebat9@gmail.com>
2013-10-21 23:41:52 -0300

    login style

--- admin/common/header.bootstrap.php
+++ admin/common/header.bootstrap.php
@@ -4,6 +4,5 @@
 <title><? echo $GLOBALS['admin']['title']; ?></title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="stylesheet" type="text/css" href="<? echo staticLoader('resources/lib/bootstrap/css/bootstrap.min.css') ?>">
-<link rel="stylesheet" type="text/css" href="<? echo staticLoader('resources/lib/bootstrap/css/bootstrap-responsive.min.css') ?>">
-<script type="text/javascript" src="<? echo staticLoader('resources/lib/jquery/jquery.min.js') ?>"></script>
+<script type="text/javascript" src="<? echo staticLoader('resources/lib/jquery/jquery-1.min.js') ?>"></script>
 <script type="text/javascript" src="<? echo staticLoader('resources/lib/bootstrap/js/bootstrap.min.js') ?>"></script>
--- admin/login.php
+++ admin/login.php
@@ -27,35 +27,37 @@ if(isset($_GET['logout']))
 require(dirname(__FILE__).'/common/header.bootstrap.php');
 ?>
 <style type="text/css">
-body{padding:40px 0;background-color:#f5f5f5}
-.form-signin{max-width:300px;padding:19px 29px 29px;margin:0 auto 20px;background-color:#fff;border:1px solid #e5e5e5;
+body{padding:40px 0;background-color:#eee}
+.form-signin{max-width:300px;padding:20px 30px;margin:0 auto;background-color:#fff;border:1px solid #e5e5e5;
 -webkit-border-radius:5px;-moz-border-radius:5px;border-radius:5px;
--webkit-box-shadow:0 1px 2px rgba(0,0,0,.05);-moz-box-shadow:0 1px 2px rgba(0,0,0,.05);box-shadow:0 1px 2px rgba(0,0,0,.05);}
-.form-signin select,.form-signin input[type="text"],.form-signin input[type="password"]{
-font-size:16px;height:auto;margin:5px 0 15px;padding:7px 9px}
+-webkit-box-shadow:0 1px 2px rgba(0,0,0,.05);
+-moz-box-shadow:0 1px 2px rgba(0,0,0,.05);
+box-shadow:0 1px 2px rgba(0,0,0,.05);
+}
+.form-signin select,.form-signin input[type="text"],.form-signin input[type="password"]{font-size:16px;height:auto;margin:5px 0 12px;padding:7px 9px}
 </style>
 </head>
 <body>
 <div class="container">
 	<form method="post" class="form-signin">
 	    <? if(isset($error)) { ?>
-	    <div class="alert alert-error">
-            <button type="button" class="close" data-dismiss="alert">&times;</button>
-            <strong>Warning!</strong><br>No match for Username and/or Password.
+	    <div class="alert alert-danger">
+            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+            <strong>Warning!</strong><br>No match for Username / Password
         </div>
         <? } ?>
 		<legend>Login</legend>
 		<label>
 			Usuario<br>
-			<input type="text" name="name" class="input-block-level" required autofocus />
+			<input type="text" name="name" class="form-control" required autofocus />
 		</label>
 		<label>
 			Password<br>
-			<input type="password" name="pass" class="input-block-level" required />
+			<input type="password" name="pass" class="form-control" required />
 		</label>
 		<label>
 			Language<br>
-			<select name="locale" class="input-block-level">
+			<select name="locale" class="form-control">
 			<?
             foreach($GLOBALS['admin']['locale'] as $key => $lang)
             {
@@ -66,7 +68,7 @@ font-size:16px;height:auto;margin:5px 0 15px;padding:7px 9px}
 			</select>
 		</label>
 		<input type="hidden" name="act_login" value="1" />
-		<button type="submit" class="btn btn-large btn-primary">Login</button>
+		<button type="submit" class="btn btn-lg btn-primary btn-block">Login</button>
 	</form>
 </div>
 </body>
