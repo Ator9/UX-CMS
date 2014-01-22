1c2b03532eecc99ecd86c052c4ec0d27c3cf7267
Ator9 <sebat9@gmail.com>
2014-01-22 18:43:14 -0300

    permisos

--- modules/admins/config.default.php
+++ modules/admins/config.default.php
@@ -3,6 +3,7 @@ $config = array(
     'enabled' => true,
     'name'    => 'Admins',
     'icon'    => 'user-detective.png',
+    'admins'  => array(), // Allowed admins (array with adminID)
     'accounts_config' => array( // config => description
         'api_user'    => 'api user description',
         'api_pass'    => 'api pass description', 
