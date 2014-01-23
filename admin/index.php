ba923b63eadcb63f93871bd289a2b11322a2efa5
Ator9 <sebat9@gmail.com>
2014-01-23 09:49:27 -0300

    custom js index.php

--- admin/index.php
+++ admin/index.php
@@ -61,7 +61,7 @@ Ext.application({
             var icon  = (type) ? Ext.Msg.INFO : Ext.Msg.ERROR;
             Ext.Msg.show({ title: title, msg: msg, buttons: Ext.Msg.OK, icon: icon });
         }
-        <? echo $GLOBALS['admin']['js_functions']; ?>
+        <? echo $GLOBALS['admin']['custom_js']; ?>
     
         Ext.create('Ext.container.Viewport', {
             layout: 'border',
