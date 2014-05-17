4a921de024559a530f4c08d785a1d0a79c6e50d2
Ator9 <sebat9@gmail.com>
2014-05-17 13:33:54 -0300

    developers

--- modules/admins/admin/view/Developers.js
+++ modules/admins/admin/view/Developers.js
@@ -2,23 +2,24 @@ Ext.define('admins.view.Developers', {
     extend: 'Ext.tab.Panel',
     
     initComponent: function() {
+        var items = []
 
-        var docs = Ext.create('Ext.ux.IFrame', {
+        items.push(Ext.create('Ext.ux.IFrame', {
             title: 'Documentation',
             src: Admin.getModulesUrl(this)+'/documentation.php'
-        });
+        }));
 
-        var php = Ext.create('Ext.ux.IFrame', {
+        items.push(Ext.create('Ext.ux.IFrame', {
             title: 'PHP Info',
             src: Admin.getModulesUrl(this)+'/phpinfo.php'
-        });
+        }));
 
-        var db = Ext.create('Ext.ux.IFrame', {
+        items.push(Ext.create('Ext.ux.IFrame', {
             title: 'phpMiniAdmin',
             src: Admin.getModulesUrl(this)+'/phpminiadmin.php'
-        });
+        }));
 
-        this.items = [ docs, php, db ];
+        this.items = items;
         this.callParent(arguments);
     }
 });
