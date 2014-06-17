0dd2baaf6d6819252f2a5a1b9c99286f4ceb62ed
Ator9 <sebat9@gmail.com>
2014-06-17 20:52:17 -0300

    foreign keys

--- modules/admins/create.sql
+++ modules/admins/create.sql
@@ -17,10 +17,7 @@ CREATE TABLE IF NOT EXISTS `admins` (
   PRIMARY KEY (`adminID`),
   UNIQUE KEY `username` (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-INSERT INTO `admins` (`adminID`, `username`, `password`, `superuser`, `active`, `deleted`)
-VALUES (1, 'admin', 'test', 'Y', 'Y', 'N');
-
+INSERT INTO `admins` (`adminID`, `username`, `password`, `superuser`, `active`, `deleted`) VALUES (1, 'admin', 'test', 'Y', 'Y', 'N');
 
 CREATE TABLE IF NOT EXISTS `admins_logs` (
   `logID` int(10) unsigned NOT NULL AUTO_INCREMENT,
@@ -32,7 +29,19 @@ CREATE TABLE IF NOT EXISTS `admins_logs` (
   `date_created` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
   `date_updated` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`logID`),
-  KEY `adminID` (`adminID`)
+  KEY `adminID` (`adminID`),
+  CONSTRAINT `FK_admins_logs_admins` FOREIGN KEY (`adminID`) REFERENCES `admins` (`adminID`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `admins_roles` (
+  `roleID` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL DEFAULT '',
+  `permission` text NOT NULL,
+  `adminID_created` mediumint(8) unsigned NOT NULL DEFAULT '0',
+  `adminID_updated` mediumint(8) unsigned NOT NULL DEFAULT '0',
+  `date_created` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `date_updated` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`roleID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
@@ -56,7 +65,10 @@ CREATE TABLE IF NOT EXISTS `partners_admins` (
   `adminID_updated` mediumint(8) unsigned NOT NULL DEFAULT '0',
   `date_created` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
   `date_updated` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
-  UNIQUE KEY `partnerID_adminID` (`partnerID`,`adminID`)
+  UNIQUE KEY `partnerID_adminID` (`partnerID`,`adminID`),
+  KEY `FK_partners_admins_admins` (`adminID`),
+  CONSTRAINT `FK_partners_admins_admins` FOREIGN KEY (`adminID`) REFERENCES `admins` (`adminID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `FK_partners_admins_partners` FOREIGN KEY (`partnerID`) REFERENCES `partners` (`partnerID`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `partners_configs` (
@@ -67,19 +79,6 @@ CREATE TABLE IF NOT EXISTS `partners_configs` (
   `adminID_updated` mediumint(8) unsigned NOT NULL DEFAULT '0',
   `date_created` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
   `date_updated` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
-  UNIQUE KEY `partnerID_name` (`partnerID`,`name`)
+  UNIQUE KEY `partnerID_name` (`partnerID`,`name`),
+  CONSTRAINT `FK_partners_configs_partners` FOREIGN KEY (`partnerID`) REFERENCES `partners` (`partnerID`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
-CREATE TABLE IF NOT EXISTS `admins_roles` (
-  `roleID` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) NOT NULL DEFAULT '',
-  `permission` text NOT NULL,
-  `adminID_created` mediumint(8) unsigned NOT NULL DEFAULT '0',
-  `adminID_updated` mediumint(8) unsigned NOT NULL DEFAULT '0',
-  `date_created` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `date_updated` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`roleID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
