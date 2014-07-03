bea7f3a2938d8bee42b98a685bfae25c643f4ba1
seba <sgasparri@adverit.com>
2014-07-03 17:16:54 -0300

    docs

--- modules/admins/admin/git.php
+++ modules/admins/admin/git.php
@@ -12,21 +12,24 @@ require(ROOT.'/admin/common/header.bootstrap.php');
     <h2>Commands</h2>
     <ul>
         <li>
-            <b>Clone Repository</b>
-            <pre>git clone https://github.com/Ator9/CMS my_project_folder/</pre>
-        </li>
-        <li>
             <b>Pull Changes</b>
             <pre>git pull</pre>
         </li>
         <li>
             <b>Commit NEW file (not tracked)</b>
-            <pre>git add file.xxx<br>git commit -m "comment"<br>git push</pre>
+            <pre>git add file.xxx<br>git commit -m "comment"</pre>
         </li>
         <li>
             <b>Commit all modified files (tracked)</b>
-            <pre>git commit -am "comment"<br>git push</pre>
+            <pre>git commit -am "comment"</pre>
         </li>
+        <li>
+            <b>Push / Push Online</b>
+            <pre>git push<br>git push online master</pre>
+        </li>
+    </ul>
+    <h2>Extras</h2>
+    <ul>
         <li>
             <b>Reset file changes</b>
             <pre>git checkout -- file.xxx</pre>
@@ -36,8 +39,8 @@ require(ROOT.'/admin/common/header.bootstrap.php');
             <pre>git status</pre>
         </li>
         <li>
-            <b>View Log</b>
-            <pre>git log</pre>
+            <b>View Log / Shortlog</b>
+            <pre>git log<br>git shortlog</pre>
         </li>
     </ul>
     <h2>Remotes</h2>
