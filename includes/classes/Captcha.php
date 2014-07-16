2786ab03ab511df9ef9ee14c4343405be91dbce2
Ator9 <sebat9@gmail.com>
2014-07-16 17:17:28 -0300

    captcha update

--- includes/classes/Captcha.php
+++ includes/classes/Captcha.php
@@ -15,32 +15,43 @@
 
 class Captcha
 {
-    public $publickey  = ''; // You got this from the signup page
-	public $privatekey = ''; // You got this from the signup page
-	public $lang  	   = 'es';
-	public $theme 	   = 'white';
+    public $public_key  = ''; // You got this from the signup page
+	public $private_key = ''; // You got this from the signup page
+	public $lang  	    = 'es';
+	public $theme 	    = 'white';
+	
+	
+	public function __construct($config = array())
+    {
+        foreach($config as $key => $value)
+        {
+            if(isset($this->$key)) $this->$key = $value;
+        }
+    }
+    
 
 	public function get()
 	{
 		$js = '<script type="text/javascript">var RecaptchaOptions={lang:"'.$this->lang.'",theme:"'.$this->theme.'"};</script>';
-		return $js.recaptcha_get_html($this->publickey);
+		return $js.recaptcha_get_html($this->public_key);
 	}
 
 
 	public function getScript($id)
 	{
 		$js = '<script type="text/javascript">
-			       Recaptcha.create("'.$this->publickey.'","'.$id.'",{lang:"'.$this->lang.'",theme:"'.$this->theme.'",callback:Recaptcha.focus_response_field});
+			       Recaptcha.create("'.$this->public_key.'","'.$id.'",{lang:"'.$this->lang.'",theme:"'.$this->theme.'",callback:Recaptcha.focus_response_field});
 			   </script>';
 		return $js;
 	}
 
 
-	public function check()
+	public function check($challenge = '', $response = '')
 	{
-		$resp = recaptcha_check_answer($this->privatekey, $_SERVER["REMOTE_ADDR"], $_POST["recaptcha_challenge_field"], $_POST["recaptcha_response_field"]);
-		if($resp->is_valid) return true;
-		return false;
+	    if($challenge == '') $challenge = $_POST['recaptcha_challenge_field'];
+	    if($response == '')  $response  = $_POST['recaptcha_response_field'];
+		$resp = recaptcha_check_answer($this->private_key, $_SERVER['REMOTE_ADDR'], $challenge, $response);
+		return $resp->is_valid;
 	}
 }
 
