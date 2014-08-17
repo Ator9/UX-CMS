d6600ebba09bf2d1f0dfd48a52b8d7c0404151d1
Ator9 <sebat9@gmail.com>
2014-08-17 17:52:59 -0300

    update extjs5 doctype

--- admin/common/header.extjs5.php
+++ admin/common/header.extjs5.php
@@ -2,6 +2,7 @@
 <html>
 <head>
 <title><?php echo $GLOBALS['admin']['title']; ?></title>
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
 <link rel="stylesheet" type="text/css" href="<?php echo staticLoader('admin/resources/extjs5/packages/ext-theme-classic/build/resources/ext-theme-classic-all.css'); ?>">
 <link rel="stylesheet" type="text/css" href="<?php echo staticLoader('admin/resources/extjs.default.css'); ?>">
 <?php if(is_file(__DIR__.'/../resources/extjs.css')) { ?>
