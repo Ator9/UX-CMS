ee2d7b51c9e3f001ec0e04450caea55ee938a4c4
Ator9 <sebat9@gmail.com>
2014-09-02 10:00:18 -0300

    class admins

--- modules/admins/classes/adminsAdmin.php
+++ modules/admins/classes/adminsAdmin.php
@@ -4,20 +4,6 @@ class adminsAdmin extends ConnExtjs
     public $_debug  = false; // True to save all queries (adminsLog)
 	public $_table	= 'admins';
 	public $_index	= 'adminID';
-	public $_fields	= array('roleID',
-	                        'username',
-							'password',
-							'email',
-							'firstname',
-							'lastname',
-							'superuser',
-							'last_login',
-							'active',
-							'deleted',
-							'adminID_created', // (Reserved) Automatic usage on insert (Conn.php)
-							'adminID_updated', // (Reserved) Automatic usage on update (Conn.php)
-							'date_created',    // (Reserved) Automatic usage on insert (Conn.php)
-							'date_updated');   // (Reserved) Automatic usage on update (Conn.php)
 
 
 	// ------------------------------------------------------------------------------- //
@@ -28,7 +14,8 @@ class adminsAdmin extends ConnExtjs
         global $aSession;
 	
 		$sql = 'SELECT * FROM '.$this->_table.' 
-		        WHERE username = "'.$this->escape($user).'" AND password = "'.$this->escape($pass).'" AND active = "Y" AND deleted <> "Y"';
+		        WHERE username = "'.$this->escape($user).'" AND password = "'.$this->escape($pass).'" 
+		        AND active = "Y" '.((in_array('deleted', $this->_fields)) ? 'AND deleted <> "Y"' : '');
 		if(($rs = $this->query($sql)) && $rs->num_rows == 1)
 		{
 		    // Last Login:
