a0ca3899ab37996439057c1b6179551bcbc9ab4a
Sebastián Gasparri <sebat9@gmail.com>
2015-04-24 09:22:10 -0300

    Update adminsAdmin.php

--- modules/admins/classes/adminsAdmin.php
+++ modules/admins/classes/adminsAdmin.php
@@ -35,6 +35,32 @@ class adminsAdmin extends ConnExtjs
 		}
 		return false;
 	}
+	
+	
+		
+	
+	// Select admin list for dropdown filters:
+	public function extAdminsList()
+    {
+        $response['success'] = true;
+        
+        $sql = 'SELECT a.adminID, a.username, a.firstname, a.lastname, a.email
+                FROM '.$this->_table.' a
+                INNER JOIN partners_admins p USING (adminID)
+                WHERE p.partnerID IN ('.implode(',', array_keys($GLOBALS['admin']['data']['partners'])).')
+                GROUP BY a.adminID
+                ORDER BY username ASC';
+
+		if($rs = parent::query($sql))
+		{
+			while($row = $rs->fetch_assoc())
+			{
+				$response['data'][] = $row;
+			}
+		}
+		
+        echo json_encode($response);
+    }
 }
 
 
