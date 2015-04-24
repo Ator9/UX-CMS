79413f0e4229af26694a1cf452d3ad5d56bd2032
Sebastián Gasparri <sebat9@gmail.com>
2015-04-24 09:20:33 -0300

    Create AdminsList.js

---
+++ modules/admins/admin/store/AdminsList.js
@@ -0,0 +1,22 @@
+Ext.define('admins.store.AdminsList', {
+    extend: 'Ext.data.Store',
+    
+    fields: [ // Model
+        'adminID', 'username', 'firstname', 'lastname', 'email'
+    ],
+    pageSize: 999, // We need "infinite" to show all options in combobox (Defaults to 25)
+    remoteSort: false, // Default false (javascript sort)
+    sorters: [{ property: 'username', direction: 'ASC' }],
+    proxy: {
+        type: 'ajax',
+        simpleSortMode: true, // Default false (enable multiple sorts)
+        api: {
+            read: 'index.php?_class=adminsAdmin&_method=extAdminsList' // reader
+        },
+        reader: {
+            type: 'json',
+            root: 'data', // php response name
+            totalProperty: 'totalCount' // PagingToolbar (php response name)
+        }
+    }
+});
