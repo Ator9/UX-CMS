1a6a5078eaeb85904b652154ff2b077ce0943a25
Sebastián Gasparri <sebat9@gmail.com>
2015-06-04 23:27:34 -0300

    Update PHPMailerAutoload.php

--- includes/lib/phpmailer/PHPMailerAutoload.php
+++ includes/lib/phpmailer/PHPMailerAutoload.php
@@ -33,7 +33,7 @@ function PHPMailerAutoload($classname)
 if (version_compare(PHP_VERSION, '5.1.2', '>=')) {
     //SPL autoloading was introduced in PHP 5.1.2
     if (version_compare(PHP_VERSION, '5.3.0', '>=')) {
-        spl_autoload_register('PHPMailerAutoload', true, true);
+        spl_autoload_register('PHPMailerAutoload', true);
     } else {
         spl_autoload_register('PHPMailerAutoload');
     }
