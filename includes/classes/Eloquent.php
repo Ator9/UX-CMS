ee39dadc5bfdb2c24e4afde6a68881347bf59990
Ator9 <sebat9@gmail.com>
2015-07-28 21:31:53 -0300

    eloquent

---
+++ includes/classes/Eloquent.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Create a new Eloquent model instance
+ *
+ * @author http://laravel.com/docs/eloquent
+ */
+require_once(INCLUDES.'/lib/illuminate/database/autoload.php');
+
+class Eloquent extends Illuminate\Database\Eloquent\Model
+{
+    // Create a new database capsule manager
+    public function __construct($host=DB_HOST, $user=DB_USER, $pass=DB_PASS, $db=DB_NAME)
+    {
+        $capsule = new Illuminate\Database\Capsule\Manager;
+        $capsule->addConnection(array(
+            'driver'    => 'mysql',
+            'host'      => $host,
+            'database'  => $db,
+            'username'  => $user,
+            'password'  => $pass,
+            'charset'   => 'utf8',
+            'collation' => 'utf8_unicode_ci'
+        ));
+        $capsule->bootEloquent();
+
+        parent::__construct();
+    }
+}
