bcfd4b2b8989b125a344d54423cbe0f886a4a1c9
Sebastián Gasparri <sebat9@gmail.com>
2015-09-07 09:45:41 -0300

    Update Webhook.php

--- includes/classes/Webhook.php
+++ includes/classes/Webhook.php
@@ -49,7 +49,7 @@ class Webhook
         echo 'Starting Git Sync...<br><br>';
         echo 'Current User: '.exec('whoami').'<br><br>';
 
-        echo $command = 'cd '.$this->git_dir.' && git --work-tree=/var/www/acc.adverit.com/web --git-dir=/var/www/acc.adverit.com/private pull';
+        echo $command = 'git --work-tree=/var/www/acc.adverit.com/web --git-dir=/var/www/acc.adverit.com/private pull<br>';
         passthru($command);
     }
 }
