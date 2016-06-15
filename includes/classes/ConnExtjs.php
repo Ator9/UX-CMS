38a4e855b775e0828a851598394e760d1d56766a
Sebastián Gasparri <sebat9@gmail.com>
2016-06-15 13:19:33 -0300

    Update ConnExtjs.php

--- includes/classes/ConnExtjs.php
+++ includes/classes/ConnExtjs.php
@@ -1,7 +1,7 @@
 <?php
 class ConnExtjs extends Conn
 {
-    public $_debug  = false;   // True to save all queries (adminsLog)
+    public $_debug  = false;   // True to save all queries in adminsLog
 	public $_table	= '';      // Table name
 	public $_index	= '';      // Table primary Key
 	public $_fields	= array(); // Table columns (auto filled with "getColumns()" if not set)
