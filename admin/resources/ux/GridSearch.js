77df4aa4130e7cf99f8f8ce1951ea803ec002251
Sebastián Gasparri <sebat9@gmail.com>
2016-06-22 11:18:48 -0300

    Update GridSearch.js

--- admin/resources/ux/GridSearch.js
+++ admin/resources/ux/GridSearch.js
@@ -4,43 +4,50 @@
  * @author Sebastián Gasparri
  * http://www.linkedin.com/in/sgasparri
  *
- * Usage: 
+ * Usage:
  * Ext.create('Ext.ux.GridSearch') // Automatic store set
  * Ext.create('Ext.ux.GridSearch', { store: this.store })
  * Ext.create('Ext.ux.GridSearch', { store: this.store, columns: [ 'adminID', 'username' ] })
  *
  */
- 
+
 Ext.define('Ext.ux.GridSearch', {
     extend: 'Ext.toolbar.Toolbar',
 
     border: false,
     inputWidth: 200,
+    inputType: 'search',
     paramName: 'search',
     columns: [], // Search columns. Empty = database index
     tooltip: '', // Custom textfield tooltip. If not set, it will use columns data.
-    
+
     initComponent: function() {
-        this.field = Ext.create('Ext.form.TwinTriggerField', {
+        this.field = Ext.create('Ext.form.field.Text', {
             width          : this.inputWidth,
             inputAttrTpl   : 'data-qtip="'+((this.tooltip != '') ? this.tooltip : this.columns)+'"',
-            trigger1Cls    : 'x-form-clear-trigger',
-            trigger2Cls    : 'x-form-search-trigger',
-            onTrigger1Click: Ext.bind(this.onTriggerClear, this),
-            onTrigger2Click: Ext.bind(this.onTriggerSearch, this)
+            triggers: {
+                clear: {
+                    cls: 'x-form-clear-trigger',
+                    weight: -1,
+                    scope: this,
+                    handler: function(obj) {
+                        obj.setValue('');
+                        obj.focus();
+
+                        delete(this.store.getProxy().extraParams[this.paramName]);
+                        this.store.loadPage(1); // Reload
+                    }
+                },
+                search: {
+                    cls: 'x-form-search-trigger',
+                    scope: this,
+                    handler: this.onTriggerSearch
+                }
+            }
         });
         this.items = [ Admin.t('Search'), this.field ];
-        
+
         this.callParent(arguments);
-    },
-    
-    // Clear function:
-    onTriggerClear: function() {
-        this.field.setValue('');
-        this.field.focus();
-        delete(this.store.getProxy().extraParams[this.paramName]);
-        
-        this.store.loadPage(1); // Reload
     },
 
     // Search function:
@@ -57,7 +64,7 @@ Ext.define('Ext.ux.GridSearch', {
 
     onRender: function() {
         if(!this.store) this.store = this.up('grid').getStore(); // Uses grid store if not set
-    
+
         // Install key map ENTER:
         new Ext.util.KeyMap({
             target: this.field.id,
@@ -65,7 +72,7 @@ Ext.define('Ext.ux.GridSearch', {
             fn: this.onTriggerSearch,
             scope: this
         });
-        
+
         this.callParent(arguments);
     }
 });
