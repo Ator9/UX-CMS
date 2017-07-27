5bea4d9219ddcdc4cb02bfafe434379a8ee6a8a3
Sebastián Gasparri <sebat9@gmail.com>
2017-07-26 21:51:08 -0300

    Revert "Clean Facebook"
    This reverts commit 21de2e51c76ab932bcc878b195363dcc59c973dc.

---
+++ includes/classes/Facebook.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ *
+ * Required PHP: 
+ * - extension=php_curl.dll
+ * - extension=php_openssl.dll (usado en varias peticiones a facebook)
+ *
+ *
+ */
+
+require(INCLUDES.'/lib/facebook/v3/src/facebook.php');
+
