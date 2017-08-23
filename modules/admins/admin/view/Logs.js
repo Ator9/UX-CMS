f00fb8b59aed2159c5f00033c3de79d8039008f2
Sebastián Gasparri <2809743+Ator9@users.noreply.github.com>
2017-08-23 10:38:23 -0300

    Update Logs.js

--- modules/admins/admin/view/Logs.js
+++ modules/admins/admin/view/Logs.js
@@ -1,8 +1,15 @@
 Ext.define('admins.view.Logs', {
     extend: 'Ext.grid.Panel',
     
+    config: {
+        module: 'admins',
+        phpclass: 'adminsLog',
+        primaryID: 'logID'
+    },
+    
+    // Renderers:
     textFormat: function(value, metadata) {
-        return '<div style="white-space:normal;">'+value+'</div>';
+        return '<div style="white-space:normal">'+value+'</div>';
     },
     
     initComponent: function() {
@@ -30,9 +37,9 @@ Ext.define('admins.view.Logs', {
             listeners: { scope: this, select: this.onSelectArea }
         });
         
-        this.store = Ext.create('admins.store.Logs').load(); // Store + Load
+        this.store = Ext.create(this.self.getName().replace('view','store')).load(); // Store + Load
         this.tbar = [
-            Ext.create('Ext.ux.GridSearch', { columns: [ 'logID', 'classname', 'task', 'comment' ] }), 
+            Ext.create('Ext.ux.GridSearch', { columns: [ this.primaryID, 'classname', 'task', 'comment' ] }), 
             Admin.t('Date from'), {
                 xtype: 'datefield',
                 name: 'date_from',
@@ -72,13 +79,13 @@ Ext.define('admins.view.Logs', {
             '|', Ext.create('Ext.ux.GridExport', { csvZip: true }) 
         ];
         this.columns = [
-            { header: 'ID', dataIndex: 'logID', width: 80 },
-            { header: 'Classname', dataIndex: 'classname' },
-            { header: 'Task', dataIndex: 'task' },
-            { header: 'Comment', dataIndex: 'comment', flex: 1, renderer: this.textFormat },
+            { header: Admin.t('ID'), dataIndex: 'logID', width: 80 },
+            { header: Admin.t('Classname'), dataIndex: 'classname' },
+            { header: Admin.t('Task'), dataIndex: 'task' },
+            { header: Admin.t('Comment'), dataIndex: 'comment', flex: 1, renderer: this.textFormat },
             { header: Admin.t('User'), dataIndex: 'username' },
-            { header: 'IP', dataIndex: 'ip' },
-            { header: 'Date', dataIndex: 'date_created', xtype: 'datecolumn', format:'d/m/Y H:i:s', width: 120 }
+            { header: Admin.t('IP'), dataIndex: 'ip' },
+            { header: Admin.t('Date'), dataIndex: 'date_created', xtype: 'datecolumn', format:'d/m/Y H:i:s', width: 120 }
         ];
         this.viewConfig = { enableTextSelection: true };
         this.bbar = Ext.create('Ext.toolbar.Paging', { store: this.store, displayInfo: true });
